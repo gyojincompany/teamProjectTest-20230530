89aaacd0dab02d6dbf98b99f2cf0ce94c90bb880
ICI <ICI@DESKTOP-EA5M9P6>
2023-05-30 14:13:52 +0900

    기본 설정 완료

---
+++ src/main/java/com/gyojincompany/board/TeamProjectTest20230530Application.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProjectTest20230530Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamProjectTest20230530Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyojincompany/board/TeamProjectTest20230530ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProjectTest20230530ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
